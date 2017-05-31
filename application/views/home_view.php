5256338d3a30996e01862b6cc303cc65fed67b45
ifer777 <ivan.paz@itsae.edue.ec>
2017-05-31 18:46:11 -0500

    change database_name

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -50,7 +50,7 @@
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><?php ?></a></li>
+        <li><a href="#"><?php $fullname ?></a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
