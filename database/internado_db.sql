2b9d600c7632730d7c8ae524a84ba0833ac98034
ifer777 <ivan.paz@itsae.edue.ec>
2017-06-13 14:10:40 -0500

    update_database

--- database/internado_db.sql
+++ database/internado_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2017 a las 12:36:49
--- Versión del servidor: 10.1.21-MariaDB
--- Versión de PHP: 5.6.30
+-- Tiempo de generación: 13-06-2017 a las 18:58:34
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sistema_internado`
+-- Base de datos: `internado`
 --
 
 -- --------------------------------------------------------
@@ -79,14 +79,22 @@ CREATE TABLE `estudiantes` (
 --
 
 CREATE TABLE `login` (
-  `id_login` int(11) NOT NULL,
-  `tipo_login` varchar(45) NOT NULL,
-  `nombre_lo` varchar(45) NOT NULL,
-  `apellido_lo` varchar(45) NOT NULL,
-  `clave` varchar(45) NOT NULL,
-  `fecha_lo` varchar(45) NOT NULL,
-  `estado_lo` varchar(45) NOT NULL
+  `id` int(11) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `fullname` varchar(45) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `date` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id`, `tipo`, `fullname`, `email`, `password`, `date`, `status`) VALUES
+(1, 'admin', 'Ivan Fernando Paz', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', '', 'activo'),
+(2, '', 'Raymond Amangandi', 'raymond.amangandi@itsae.edu.ec', '25d55ad283aa400af464c76d713c07ad', '', '');
 
 -- --------------------------------------------------------
 
@@ -181,7 +189,7 @@ ALTER TABLE `estudiantes`
 -- Indices de la tabla `login`
 --
 ALTER TABLE `login`
-  ADD PRIMARY KEY (`id_login`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `religion`
@@ -231,7 +239,7 @@ ALTER TABLE `estudiantes`
 -- AUTO_INCREMENT de la tabla `login`
 --
 ALTER TABLE `login`
-  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `religion`
 --
@@ -262,7 +270,7 @@ ALTER TABLE `salud`
 ALTER TABLE `estudiantes`
   ADD CONSTRAINT `fk_estudiantes_adventistas1` FOREIGN KEY (`adventistas_id_adventistas`) REFERENCES `adventistas` (`id_adventistas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_estudiantes_carrera1` FOREIGN KEY (`carrera_id_carrera`) REFERENCES `carrera` (`id_carrera`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_estudiantes_login1` FOREIGN KEY (`login_id_login`) REFERENCES `login` (`id_login`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_estudiantes_login1` FOREIGN KEY (`login_id_login`) REFERENCES `login` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_estudiantes_religion1` FOREIGN KEY (`religion_id_religion`) REFERENCES `religion` (`id_religion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_estudiantes_representante1` FOREIGN KEY (`representante_id_representante`) REFERENCES `representante` (`id_representante`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_estudiantes_salud` FOREIGN KEY (`salud_id_salud`) REFERENCES `salud` (`id_salud`) ON DELETE NO ACTION ON UPDATE NO ACTION;
