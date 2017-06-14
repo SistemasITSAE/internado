30409b18d881f2235bf22ded4cb7f49fead466e3
jeffersonmena <jefferson.mena@itsae.edu.ec>
2017-06-13 21:30:11 -0500

    views

---
+++ application/views/registros/Principal.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.css" class="rel">
+  <link rel="stylesheet" href="estilo.css" class="rel">
+	<title>Sistema de Gestion de Internados</title>
+</head>
+<body>
+
+ <div class="row">
+  
+   <div class="col-md-2">
+    <div class="sidebar-nav">
+      <div class="navbar navbar-default" role="navigation">
+              <img src="photo.png" alt="" width="200" height="150">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="datosgenerales.php">Datos Personales</a></li>
+            <li><a href="informacionfinanciera.php">Imformacion Financiera</a></li>
+            <li><a href="antecedentesreligiosos.php">Antecedentes Religiosos</a></li>
+            <li><a href="soloadventistas.php">Solo Adventista</a></li>
+            <li><a href="datosdelospadres.php">Datos de los padres</a></li>
+            <li><a href="datosdesalud.php">Datos de Salud</a></li>
+
+          </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-10">
+<h1 align="center">DEPARTAMENTO DE BIENESTAR ESTUDIANTIL</h1>
+<h2 align="center">FICHA INFORMATIVA DE RESIDENTES</h2>
+<h3 align="center">ITSAE</h3>
+</div>
+
+</div>
+
+
+
+</body>
+</html>
---
+++ application/views/registros/soloadventistas.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/bootstrap.css" class="rel">
+  <link rel="stylesheet" href="estilo.css" class="rel">
+  <title>Sistema de Gestion de Internados</title>
+</head>
+<body>
+
+ <div class="row">
+  
+   <div class="col-md-2">
+    <div class="sidebar-nav">
+      <div class="navbar navbar-default" role="navigation">
+              <img src="photo.png" alt="" width="200" height="150">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="datosgenerales.php">Datos Personales</a></li>
+            <li><a href="informacionfinanciera.php">Imformacion Financiera</a></li>
+            <li><a href="antecedentesreligiosos.php">Antecedentes Religiosos</a></li>
+            <li><a href="soloadventistas.php">Solo Adventista</a></li>
+            <li><a href="datosdelospadres.php">Datos de los padres</a></li>
+            <li><a href="datosdesalud.php">Datos de Salud</a></li>
+          </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-10">
+<h1 align="center">DEPARTAMENTO DE BIENESTAR ESTUDIANTIL</h1>
+<h2 align="center">FICHA INFORMATIVA DE RESIDENTES</h2>
+<h3 align="center">ITSAE</h3>
+  <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-4 col-md-offset-1 col-sm-8 col-sm-offset-2">
+            <div class="panel panel-info" >
+              <div class="panel-heading">
+               <div class="panel-title" align="center">solo bautizados</div>
+               </div>
+               <div style="padding-top:30px" class="panel-body" >
+                
+                 <div class="col-sm-12" align="center">
+                 <div class="form-inline">
+                 <div class="form-group">
+                 <label for="">Iglesia a la que Asiste:</label>
+                 <input name="remitosucursal" id="remitosucursal" type="text" required class="form-control">
+               </div>
+               </div> <br>
+               </div> <br>
+
+               <br>
+
+               <div class="col-sm-12" align="center">
+                 <div class="form-inline">
+                 <div class="form-group">
+                 <label for="">Distrito:</label>
+                 <input name="remitosucursal" id="remitosucursal" type="text" required class="form-control">
+               </div>
+               </div> <br>
+               </div> <br>
+
+               <br>
+
+               <div class="col-sm-12" align="center">
+                 <div class="form-inline">
+                 <div class="form-group">
+                 <label for="">Mision:</label>
+                 <input name="remitosucursal" id="remitosucursal" type="text" required class="form-control">
+               </div>
+               </div> <br>
+               </div> <br>
+
+               <br>
+
+               <div class="col-sm-12" align="center">
+                 <div class="form-inline">
+                 <div class="form-group">
+                 <label for="">Cargos desempeñados:</label>
+                 <input name="remitosucursal" id="remitosucursal" type="text" required class="form-control">
+               </div>
+               </div> <br>
+               </div> <br>
+
+               <br>
+
+               <div class="col-sm-12" align="center">
+                 <div class="form-inline">
+                 <div class="form-group">
+                 <label for="">Familiares Adventista:</label>
+                 <input name="remitosucursal" id="remitosucursal" type="text" required class="form-control">
+               </div>
+               </div> <br>
+               </div> <br>
+</div>
+
+</div>
+
+
+
+</body>
+</html>
