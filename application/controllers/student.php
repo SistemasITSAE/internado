463d7ceb8cc4656cd95f34b025d6522e1c8a21c1
ifer777 <ivan.paz@itsae.edu.ec>
2017-06-19 17:37:51 -0500

    update_app

--- application/controllers/student.php
+++ application/controllers/student.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class student extends CI_Controller{
-
-	public function index()
-	{
-		echo 'Esta es la nueva vista';
-		}
-
-	public function student_view(){
-
-		$this->load->view('student_view');
-	}
-}
-=======
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -31,4 +14,3 @@ class student extends CI_Controller{
 				
 	}
 }
->>>>>>> 815ea5f7d11d0b87c1f2d97ecbd99859d15f37f2
