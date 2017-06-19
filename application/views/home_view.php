5539b9297b524df4ee644c2da703a19d450e13b8
ifer777 <ivan.paz@itsae.edu.ec>
2017-06-18 23:01:58 -0500

    update styles

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,20 +1,22 @@
   <html>
     <head>
-      <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-     <link type="text/css" rel="stylesheet" href="<?php echo base_url('/assets/css/bootstrap.min.css') ?>"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="<?php echo base_url('/assets/css/style.css') ?>"  media="screen,projection"/>
-
+    <?php include 'links.php' ?>
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <title>Administrator Area</title>
     </head>
 
     <body>
-      <?php include 'header.php';?>
-      
+      <div id="wrapper">
+        <?php include 'header.php';?> 
+        <div id="page-wrapper">
+           <?php include 'side.php' ?>
+      <h1>welcome</h1>
+        </div>
+       
       <?php include 'footer.php';?>
+      </div>
+      <?php include 'scripts.php' ?>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="<?php echo base_url('/assets/js/jquery.min.js') ?> "></script>
       <script type="text/javascript" src="<?php echo base_url('/assets/js/bootstrap.min.js') ?> "></script>
---
+++ application/views/links.php
@@ -0,0 +1,25 @@
+<!--Import Google Icon Font-->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="<?php echo base_url('/assets/css/bootstrap.min.css') ?>"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="<?php echo base_url('/assets/css/style.css') ?>"  media="screen,projection"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/panel-admin.css'); ?>">
+<!--external css-->
+<!-- font icon -->
+<link href="css/elegant-icons-style.css" rel="stylesheet" />
+<link href="css/font-awesome.min.css" rel="stylesheet" />    
+<!-- full calendar css-->
+<link href="assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
+<link href="assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
+<!-- easy pie chart-->
+<link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type=text/css" media="screen"/>
+<!-- owl carousel -->
+<link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+<link href="css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+<!-- Custom styles -->
+<link rel="stylesheet" href="css/fullcalendar.css">
+<link href="css/widgets.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/style-responsive.css" rel="stylesheet" />
+<link href="css/xcharts.min.css" rel=" stylesheet">	
+<link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
