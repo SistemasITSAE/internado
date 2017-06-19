5c093826a960165d7e64d15ae41b0e0b1143a64c
Ivan Fernando Paz <ivan.paz@itsae.edu.ec>
2017-06-19 00:38:48 -0500

    update-app (#10)

--- application/views/scripts.php
+++ application/views/scripts.php
@@ -1,49 +1,49 @@
 <!-- javascripts -->
-  <script src="js/jquery.js"></script>
-	<script src="js/jquery-ui-1.10.4.min.js"></script>
-    <script src="js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
+  <script src="<?php echo base_url('/assets/js/jquery.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery-ui-1.10.4.min.js')?>"></script>
+    <script src="<?php echo base_url('/assets/js/jquery-1.8.3.min.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('/assets/js/jquery-ui-1.9.2.custom.min.js')?>"></script>
     <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('/assets/js/bootstrap.min.js')?>"></script>
     <!-- nice scroll -->
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="<?php echo base_url('/assets/js/jquery.scrollTo.min.js')?>"></script>
+    <script src="<?php echo base_url('/assets/js/jquery.nicescroll.js')?>" type="text/javascript"></script>
     <!-- charts scripts -->
-    <script src="assets/jquery-knob/js/jquery.knob.js"></script>
-    <script src="js/jquery.sparkline.js" type="text/javascript"></script>
-    <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-    <script src="js/owl.carousel.js" ></script>
+    <script src="<?php echo base_url('/assets/assets/jquery-knob/js/jquery.knob.js')?>"></script>
+    <script src="<?php echo base_url('/assets/js/jquery.sparkline.js')?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('/assets/js/jquery-easy-pie-chart/jquery.easy-pie-chart.js')?>"></script>
+    <script src="<?php echo base_url('/assets/js/owl.carousel.js')?>" ></script>
     <!-- jQuery full calendar -->
-    <<script src="js/fullcalendar.min.js"></script> <!-- Full Google Calendar - Calendar -->
-	<script src="assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
+    <<script src="<?php echo base_url('/assets/js/fullcalendar.min.js')?>"></script> <!-- Full Google Calendar - Calendar -->
+	<script src="<?php echo base_url('/assets/fullcalendar/fullcalendar/fullcalendar.js')?>"></script>
     <!--script for this page only-->
-    <script src="js/calendar-custom.js"></script>
-	<script src="js/jquery.rateit.min.js"></script>
+    <script src="<?php echo base_url('/assets/js/calendar-custom.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery.rateit.min.js')?>"></script>
     <!-- custom select -->
-    <script src="js/jquery.customSelect.min.js" ></script>
-	<script src="assets/chart-master/Chart.js"></script>
-   
+    <script src="<?php echo base_url('/assets/js/jquery.customSelect.min.js')?>" ></script>
+	<script src="<?php echo base_url('/assets/chart-master/Chart.js')?>"></script>
     <!--custome script for all page-->
-    <script src="js/scripts.js"></script>
+    <script src="<?php echo base_url('/assets/js/scripts.js')?>"></script>
     <!-- custom script for this page-->
-    <script src="js/sparkline-chart.js"></script>
-    <script src="js/easy-pie-chart.js"></script>
-	<script src="js/jquery-jvectormap-1.2.2.min.js"></script>
-	<script src="js/jquery-jvectormap-world-mill-en.js"></script>
-	<script src="js/xcharts.min.js"></script>
-	<script src="js/jquery.autosize.min.js"></script>
-	<script src="js/jquery.placeholder.min.js"></script>
-	<script src="js/gdp-data.js"></script>	
-	<script src="js/morris.min.js"></script>
-	<script src="js/sparklines.js"></script>	
-	<script src="js/charts.js"></script>
-	<script src="js/jquery.slimscroll.min.js"></script>
+    <script src="<?php echo base_url('/assets/js/sparkline-chart.js')?>"></script>
+    <script src="<?php echo base_url('/assets/js/easy-pie-chart.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery-jvectormap-1.2.2.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery-jvectormap-world-mill-en.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/xcharts.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery.autosize.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery.placeholder.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/gdp-data.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/morris.min.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/sparklines.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/charts.js')?>"></script>
+	<script src="<?php echo base_url('/assets/js/jquery.slimscroll.min.js')?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('/assets/js/jquery.min.js')?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('/assets/js/bootstrap.min.js') ?> "></script>
   <script>
-
       //knob
       $(function() {
         $(".knob").knob({
-          'draw' : function () { 
+          'draw' : function () {
             $(this.i).val(this.cv + '%')
           }
         })
@@ -65,7 +65,6 @@
       $(function(){
           $('select.styled').customSelect();
       });
-	  
 	  /* ---------- Map ---------- */
 	$(function(){
 	  $('#map').vectorMap({
@@ -83,5 +82,4 @@
 	    }
 	  });
 	});
-
   </script>
--- application/views/side.php
+++ application/views/side.php
@@ -4,9 +4,9 @@
               <!-- sidebar menu start-->
               <ul class="sidebar-menu">                
                   <li class="active">
-                      <a class="" href="index.html">
+                      <a class="" href="#">
                           <i class="icon_house_alt"></i>
-                          <span>Dashboard</span>
+                          <span>Inicio</span>
                       </a>
                   </li>
 				  <li class="sub-menu">
@@ -16,10 +16,10 @@
                           <span class="menu-arrow arrow_carrot-right"></span>
                       </a>
                       <ul class="sub">
-                          <li><a class="" href="form_component.html">Form Elements</a></li>                          
+                          <li><a class="" href="form_component.html">Form Elements</a></li>
                           <li><a class="" href="form_validation.html">Form Validation</a></li>
                       </ul>
-                  </li>       
+                  </li>
                   <li class="sub-menu">
                       <a href="javascript:;" class="">
                           <i class="icon_desktop"></i>
