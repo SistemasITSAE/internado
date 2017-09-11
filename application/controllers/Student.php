781c482f9f19d890b3329c6de078e94c0b5eb80a
jeffersonmena <jefferson.mena@itsae.edu.ec>
2017-09-10 19:11:13 -0500

    Registre complete of Students

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -1,30 +1,30 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Student extends CI_Controller{
+class Student extends CI_Controller
+{
 
-	public function index()
-	{	
-	 ECHO 'NO SE REGISTRO NINGUN DATO ';
-		}
+    public function index()
+    {
 
-	public function student_view(){
-		$this->load->view('student_view');
-			  if($this->input->post('guardar')) {
-			$this->db->register();
-				}	else{
-				$this->load->view('student_view');
+    }
 
+    public function student_view()
+    {
+        $this->load->view('student_view');
+        $this->load->model('student_model');
+        $nombres         = $this->input->get('nombres');
+        $apellidos       = $this->input->get('apellidos');
+        $lugar           = $this->input->get('lugar');
+        $fech_nacimiento = $this->input->get('fech_nacimiento');
+        $cedula          = $this->input->get('cedula');
+        $email           = $this->input->get('email');
+        $edad            = $this->input->get('edad');
+        $nacionalidad    = $this->input->get('nacionalidad');
+        $celular         = $this->input->get('celular');
 
-             
-		
-	}
-}	
+        $data = $this->student_model->registrar($nombres, $apellidos, $lugar, $fech_nacimiento, $cedula, $email, $edad, $nacionalidad, $celular);
+
+    }
+
 }
-
-/*$this->load->view('student_view');
-  if($this->input->post('guardar')) {
-			$this->db->register();
-				}	else{
-				$this->load->view('student_view');
-		}*/
