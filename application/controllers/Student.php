05c9e6be68d96b89e2db10c83043679d546dae5e
JeffersonMena <jefferson.mena@itsae.edu.ec>
2017-09-10 19:38:11 -0500

    Registre (#12)
    * registre_student
    
    * register student
    
    * Registre complete of Students

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -1,3 +1,35 @@
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Student extends CI_Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function student_view()
+    {
+        $this->load->view('student_view');
+        $this->load->model('student_model');
+        $nombres         = $this->input->get('nombres');
+        $apellidos       = $this->input->get('apellidos');
+        $lugar           = $this->input->get('lugar');
+        $fech_nacimiento = $this->input->get('fech_nacimiento');
+        $cedula          = $this->input->get('cedula');
+        $email           = $this->input->get('email');
+        $edad            = $this->input->get('edad');
+        $nacionalidad    = $this->input->get('nacionalidad');
+        $celular         = $this->input->get('celular');
+
+        $data = $this->student_model->registrar($nombres, $apellidos, $lugar, $fech_nacimiento, $cedula, $email, $edad, $nacionalidad, $celular);
+
+    }
+
+}
+
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -22,9 +54,4 @@ class Student extends CI_Controller{
 }	
 }
 
-/*$this->load->view('student_view');
-  if($this->input->post('guardar')) {
-			$this->db->register();
-				}	else{
-				$this->load->view('student_view');
-		}*/
+
--- application/models/student_model.php
+++ application/models/student_model.php
@@ -1,6 +1,50 @@
 <?PHP
 
 
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Student_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public function registrar($data)
+    {
+        $nombres         = $this->input->post('nombres');
+        $apellidos       = $this->input->post('apellidos');
+        $lugar           = $this->input->post('lugar');
+        $fech_nacimiento = $this->input->post('fech_nacimiento');
+        $cedula          = $this->input->post('cedula');
+        $email           = $this->input->post('email');
+        $edad            = $this->input->post('edad');
+        $nacionalidad    = $this->input->post('nacionalidad');
+        $celular         = $this->input->post('celular');
+        $data            = array(
+
+            'nombres'         => $nombres,
+            'apellidos'       => $apellidos,
+            'lugar'           => $lugar,
+            'fech_nacimiento' => $fech_nacimiento,
+            'cedula'          => $cedula,
+            'email'           => $email,
+            'edad'            => $edad,
+            'nacionalidad'    => $nacionalidad,
+            'celular'         => $celular,
+        );
+        $result = $this->db->insert('estudiante', $data);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student_model extends CI_Model {
@@ -37,52 +81,6 @@ function registrar(){
 
 
 
- /*   public function registr($nombres, $apellidos, $lugar, $fech_nacimiento, $email, $edad, $nacionalidad, $celular, $salud_id_salud, $carrera_id_carrera, $representante_id_representante, $religion_id_religion, $login_id_login, $adventistas_id_adventistas)
-    {
-        global $conexion;
-        $consulta="INSERT INTO estudiantes (nombres, apellidos, lugar, fech_nacimiento,cedula,email, edad , nacionalidad, celular,salud_id_salud,carrera_id_carrera,representante_id_representante,religion_id_religion,login_id_login,adventistas_id_adventistas )
-VALUES('$nombres', '$apellidos', '$lugar', CURDATE(),'$cedula','$email', '$edad' , '$nacionalidad', '$celular','$salud_id_salud','$carrera_id_carrera','$representante_id_representante','$religion_id_religion','$login_id_login','$adventistas_id_adventistas');";
-    
-            $resultado=$conexion->query($consulta);
-        return $resultado;	
-
-   $this->db->insert('student', $resultado);
-    }
-    
-    public function listar(){
-          global $conexion;
-        
-         $consulta="SELECT * FROM estudiantes";
-             
-            $resultado=$conexion->query($consulta);
-        return $resultado;
-        
-        
-    }
-     public function eliminar($id_estudiantes){
-          global $conexion;
-        
-         $consulta="DELETE FROM estudiantes WHERE id_estudiantes=$id_estudiantes";
-             
-            $resultado=$conexion->query($consulta);
-        return $resultado;
-        
-        
-    }
-    
-     
-        public function actualizar($nombres, $apellidos, $lugar, $fech_nacimiento, $cedula, $email, $edad, $nacionalidad, $celular, $salud_id_salud, $carrera_id_carrera, $representante_id_representante, $religion_id_religion, $login_id_login, $adventistas_id_adventistas)
-    {
-        
-        global $conexion;
-        $consulta="UPDATE estudiantes SET 
-
-nombres='$nombres',, apellidos='$apellidos', lugar='$lugar',, fech_nacimiento='$fech_nacimiento',cedula='$cedula',email='$email',edad='$edad', nacionalidad='$nacionalidad', celular='$celular',salud_id_salud='$salud_id_salud',,carrera_id_carrera='$carrera_id_carrera',representante_id_representante='$representante_id_representante',religion_id_religion='$religion_id_religion',login_id_login='$login_id_login',adventistas_id_adventistas='$adventistas_id_adventistas' WHERE id_estudiantes=$id_estudiantes";
-  
-            $resultado=$conexion->query($consulta);
-        return $resultado;
-
-    */
     }
     
 
@@ -91,3 +89,4 @@ nombres='$nombres',, apellidos='$apellidos', lugar='$lugar',, fech_nacimiento='$
 
 
 ?>
+
